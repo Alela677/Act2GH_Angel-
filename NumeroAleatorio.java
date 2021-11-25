b9220bbcd9523ca3545ad2f3678de5a838791173
Angel Diaz Aviles <a_angel.diaz.aviles@iespablopicasso.es>
2021-11-25 12:59:38 +0100

    v2.0

--- NumeroAleatorio.java
+++ NumeroAleatorio.java
@@ -3,13 +3,26 @@ package Tema2;
 public class NumeroAleatorio {
 
 	public static void main(String[] args) {
-		int[] array = new int [20];
-		
-		for (int i = 0; i < array.length; i++) {
-			array[i] = (int) (Math.random()*20);
-			
-		}
-		
-	}
+		int[] array = new int[20];
+		int[] num = new int[20];
 
+		int contador = 0;
+		for (int i = 0; i < num.length; i++) {
+			array[i] = (int) (Math.random() * 20);
+			System.out.print(array[i] + " ");
+			array = num;
+		}
+
+		int k = 0;
+		
+			for (int j = 0; j < array.length; j++) {
+				contador = 0;
+				if (array[k++] == num[j]) {
+					contador++;
+				}
+				System.out.println();
+				System.out.println(array[j] + " se repite en el array " + contador + " veces");
+			}
+			
+	}
 }
