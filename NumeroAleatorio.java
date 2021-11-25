4fcd80b94295d192c916ccff44a1cfaaa14ec7bd
Angel Diaz Aviles <a_angel.diaz.aviles@iespablopicasso.es>
2021-11-25 13:24:20 +0100

    Cambio añadir 30 numeros en lugar de 20

--- NumeroAleatorio.java
+++ NumeroAleatorio.java
@@ -3,11 +3,23 @@ package Tema2;
 public class NumeroAleatorio {
 
 	public static void main(String[] args) {
-		int[] array = new int [20];
+		int[] array = new int [30];
+		int[] aux;
+		int contador;
 		
+		array = aux;
+
+
 		for (int i = 0; i < array.length; i++) {
-			array[i] = (int) (Math.random()*20);
-			
+			array[i] = (int) (Math.random()*30);
+				contador = 0;
+				for (int j = 0; j < aux.length; j++) {
+					if(array[i] = aux[j]){
+						contador++;
+					}
+					
+				}
+				System.out.println(array[i] + " se repite " + contador);
 		}
 		
 	}
